059dfbe9cefe6df9812d52d3554f185ff4823e44
Tim Gaddis <timgaddis@gmail.com>
2016-01-04 12:59:19 -0700

    Changed the icons and ids of the items on the navigation drawer. Updated the onNavigationItemSelected code for the new ids.

--- app/src/main/java/planets/position/PlanetsMain.java
+++ app/src/main/java/planets/position/PlanetsMain.java
@@ -71,17 +71,21 @@ public class PlanetsMain extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_solar_ecl) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_lunar_ecl) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_lunar_occ) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_sky_pos) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_whats_up) {
+
+        } else if (id == R.id.nav_location) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_about) {
 
         }
 

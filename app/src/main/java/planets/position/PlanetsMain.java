b9d55fa87d1eecb2e5cee80a9f9244c2cabff2e3
Tim Gaddis <timgaddis@gmail.com>
2016-03-04 08:03:10 -0700

    Added copyright to source files.

--- app/src/main/java/planets/position/PlanetsMain.java
+++ app/src/main/java/planets/position/PlanetsMain.java
@@ -1,3 +1,23 @@
+/*
+ * Planet's Position
+ * A program to calculate the position of the planets in the night sky based
+ * on a given location on Earth.
+ * Copyright (C) 2016  Tim Gaddis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package planets.position;
 
 import android.Manifest;
@@ -362,6 +382,7 @@ public class PlanetsMain extends AppCompatActivity
         // Manual
         selectItem(7, true, true);
     }
+    // ********************************
 
     private void startLocationTask() {
         // No location

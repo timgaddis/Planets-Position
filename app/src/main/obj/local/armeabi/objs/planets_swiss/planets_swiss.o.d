d72c1e8b5b85c61aa73452ed78f2379f3def729c
Tim Gaddis <timgaddis@gmail.com>
2016-09-29 12:21:05 -0600

    Added uses-feature for gps to the manifest file. Update play services to version 9.6.1. Changed storage location on hard drive.

--- app/src/main/obj/local/armeabi/objs/planets_swiss/planets_swiss.o.d
+++
@@ -1,8 +0,0 @@
-/home/tgaddis/Dropbox/AndroidStudioProjects/PlanetsPosition/app/src/main/obj/local/armeabi/objs/planets_swiss/planets_swiss.o: \
- /home/tgaddis/Dropbox/AndroidStudioProjects/PlanetsPosition/app/src/main/jni/planets_swiss.c \
- /home/tgaddis/Dropbox/AndroidStudioProjects/PlanetsPosition/app/src/main/jni/swiss/swephexp.h \
- /home/tgaddis/Dropbox/AndroidStudioProjects/PlanetsPosition/app/src/main/jni/swiss/sweodef.h
-
-/home/tgaddis/Dropbox/AndroidStudioProjects/PlanetsPosition/app/src/main/jni/swiss/swephexp.h:
-
-/home/tgaddis/Dropbox/AndroidStudioProjects/PlanetsPosition/app/src/main/jni/swiss/sweodef.h:
